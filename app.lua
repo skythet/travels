43068500727aeb520cddf4663b140f9fccfdffe8
raimbek <raimbek.17@gmail.com>
2017-08-26 19:45:24 +0600

    tarantool memory increased

--- app.lua
+++ app.lua
@@ -3,7 +3,7 @@ console = require('console')
 db = require('db')
 
 box.cfg {
-    memtx_memory = (2 * 1024) * 1024 * 1024;
+    memtx_memory = (3 * 1024) * 1024 * 1024;
     memtx_dir = '/var/lib/tarantool/';
     wal_dir = '/var/lib/tarantool/';
     wal_mode = 'none';
