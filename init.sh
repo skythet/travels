beba5c1599c7d634511109bb0d51c21024ded34f
raimbek <raimbek.17@gmail.com>
2017-08-28 23:51:44 +0600

    current time fix

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@
 mkdir -p /srv/data
 unzip /tmp/data/data.zip -d /srv/data
 
-CURRENT_TIME=$(head -1 /srv/data/options.txt) \
+CURRENT_TIME=$(head -1 /tmp/data/options.txt) \
     tarantool /opt/tarantool/app.lua
